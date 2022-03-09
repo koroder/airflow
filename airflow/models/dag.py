8db815482e79f86ff34fa8dd2882fa323a4aa3bb
EricGao888 <ericgao.apache@gmail.com>
2022-03-09 21:10:34 +0800

    Add display_name field in dag model to support display dag name with national characters on web UI (#22073)

--- airflow/models/dag.py
+++ airflow/models/dag.py
@@ -250,6 +250,7 @@ class DAG(LoggingMixin):
 
     :param dag_id: The id of the DAG; must consist exclusively of alphanumeric
         characters, dashes, dots and underscores (all ASCII)
+    :param display_name: The display name for the DAG to e.g. be shown on the webserver
     :param description: The description for the DAG to e.g. be shown on the webserver
     :param schedule: Defines the rules according to which DAG runs are scheduled. Can
         accept cron string, timedelta object, Timetable, or list of Dataset objects.
@@ -360,6 +361,7 @@ class DAG(LoggingMixin):
     def __init__(
         self,
         dag_id: str,
+        display_name: Optional[str] = None,
         description: Optional[str] = None,
         schedule: ScheduleArg = NOTSET,
         schedule_interval: ScheduleIntervalArg = NOTSET,
@@ -432,6 +434,10 @@ class DAG(LoggingMixin):
             max_active_tasks = concurrency
         self._max_active_tasks = max_active_tasks
         self._pickle_id: Optional[int] = None
+        if display_name is None:
+            self._display_name = dag_id
+        else:
+            self._display_name = display_name
 
         self._description = description
         # set file location to caller source path
@@ -1128,6 +1134,10 @@ class DAG(LoggingMixin):
     @access_control.setter
     def access_control(self, value):
         self._access_control = DAG._upgrade_outdated_dag_access_control(value)
+
+    @property
+    def display_name(self) -> Optional[str]:
+        return self._display_name
 
     @property
     def description(self) -> Optional[str]:
@@ -2618,6 +2628,7 @@ class DAG(LoggingMixin):
             orm_dag.has_import_errors = False
             orm_dag.last_parsed_time = timezone.utcnow()
             orm_dag.default_view = dag.default_view
+            orm_dag.display_name = dag.display_name
             orm_dag.description = dag.description
             orm_dag.max_active_tasks = dag.max_active_tasks
             orm_dag.max_active_runs = dag.max_active_runs
@@ -2975,6 +2986,8 @@ class DagModel(Base):
     fileloc = Column(String(2000))
     # String representing the owners
     owners = Column(String(2000))
+    # Display name of the dag
+    display_name = Column(Text)
     # Description of the dag
     description = Column(Text)
     # Default view of the DAG inside the webserver
